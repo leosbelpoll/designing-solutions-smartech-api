afea7abeeb53d7c8352554639b6975a2e957350c
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-05-08 20:31:55 -0300

    Create laravel API

---
+++ api/database/migrations/2020_05_08_231401_create_forms_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('dangerous');
+            $table->float('organics');
+            $table->float('plastics');
+            $table->float('others');
+            $table->string('noPlaque');
+            $table->float('initialTravel');
+            $table->float('endTravel');
+            $table->float('purchasedGallons');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forms');
+    }
+}
