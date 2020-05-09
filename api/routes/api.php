d5f0fe2abf8c1cc85d23a04ab7f433ce8ce4a390
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-05-09 02:04:06 -0300

    Modify CORS

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware([Cors::class])->group(function () {
+Route::group(['middleware' => ['cors']], function () {
 
 // Projects routes
     Route::get('/projects', 'ProjectsController@getAll')->name('getAllProjects');
