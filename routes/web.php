7a953742fbf777d4b690960f581f8a5c06aa728d
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-05-11 19:51:04 -0300

    Fix routes REact over Laravel when browser refresh

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\View;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{path?}', function($path = null){
+    return View::make('welcome');
+})->where('path', '.*');
